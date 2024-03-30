a587fd0fab703f5eb551f3f2f818320de082bc66
Santiago Tuberquia <santituber.es@gmail.com>
2024-03-30 16:31:02 -0500

    capturas correctas

--- FrontEnd/logics_pages/logic_profile.js
+++ FrontEnd/logics_pages/logic_profile.js
@@ -5,10 +5,14 @@ fileSelectBtn.addEventListener('click', () => {
     fileInput.click();
 });
 
-fileInput.addEventListener('change', () => {
+fileInput.addEventListener('change', (e) => {
     if (e.target.files.length === 0){
         fileSelectBtn.nextElementSibling.textContent = "Ninguna imagen elegida"
         return;
     }
-    fileSelectBtn.nextElementSibling.textContent = e.target.files[0].name;
+    if (e.target.files.length === 1){
+        fileSelectBtn.nextElementSibling.textContent = e.target.files[0].name;
+    } else {
+        fileSelectBtn.nextElementSibling.textContent = `${e.target.files.length} archivos seleccionados`;
+    }
 });
